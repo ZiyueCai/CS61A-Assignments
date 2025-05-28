1fdabfbfdeac24d97e19f1c9dfceeeb42501fbfc
ZiyueCai <1972015114@qq.com>
2025-05-28 17:55:22 +0800

    Update hw01.py

--- HW/hw01/hw01.py
+++ HW/hw01/hw01.py
@@ -13,9 +13,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = __sub___
+        f = sub
     else:
-        f = __add___
+        f = add
     return f(a, b)
 
 def a_plus_abs_b_syntax_check():
